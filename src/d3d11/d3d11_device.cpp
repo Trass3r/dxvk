4d3a8ab2c670961c95c3ac596b3058040a816dbd
Trass3r <trass3r@gmail.com>
2024-11-15 02:17:46 +0100

    geometryStreams

--- src/d3d11/d3d11_device.cpp
+++ src/d3d11/d3d11_device.cpp
@@ -1984,7 +1984,7 @@ namespace dxvk {
       enabled.core.features.shaderImageGatherExtended             = VK_TRUE;
       enabled.core.features.variableMultisampleRate               = supported.core.features.variableMultisampleRate;
       enabled.extTransformFeedback.transformFeedback              = VK_TRUE;
-      enabled.extTransformFeedback.geometryStreams                = VK_TRUE;
+      enabled.extTransformFeedback.geometryStreams                = supported.core.features.geometryStreams;
     }
     
     if (featureLevel >= D3D_FEATURE_LEVEL_10_1) {
