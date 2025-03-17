7aa1c762f6a43aae0dfa0ef57626cbcb6fddee5c
Trass3r <trass3r@gmail.com>
2025-03-17 22:03:05 +0100

    use dummy resources

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -6171,9 +6171,7 @@ namespace dxvk {
                 m_cmd->track(view->image(), DxvkAccess::Read);
               }
             } else {
-              descriptorInfo.image.sampler = VK_NULL_HANDLE;
-              descriptorInfo.image.imageView = VK_NULL_HANDLE;
-              descriptorInfo.image.imageLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+              descriptorInfo.image = m_common->dummyResources().imageViewDescriptor(binding.viewType, true);
             }
           } break;
 
@@ -6196,9 +6194,7 @@ namespace dxvk {
               m_cmd->track(res.imageView->image(), (binding.access & vk::AccessWriteMask)
                 ? DxvkAccess::Write : DxvkAccess::Read);
             } else {
-              descriptorInfo.image.sampler = VK_NULL_HANDLE;
-              descriptorInfo.image.imageView = VK_NULL_HANDLE;
-              descriptorInfo.image.imageLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+              descriptorInfo.image = m_common->dummyResources().imageViewDescriptor(binding.viewType, false);
             }
           } break;
 
@@ -6232,9 +6228,7 @@ namespace dxvk {
                 m_cmd->track(res.sampler);
               }
             } else {
-              descriptorInfo.image.sampler = m_common->dummyResources().samplerHandle();
-              descriptorInfo.image.imageView = VK_NULL_HANDLE;
-              descriptorInfo.image.imageLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+              descriptorInfo.image = m_common->dummyResources().imageSamplerDescriptor(binding.viewType);
             }
           } break;
 
@@ -6249,7 +6243,7 @@ namespace dxvk {
 
               m_cmd->track(res.bufferView->buffer(), DxvkAccess::Read);
             } else {
-              descriptorInfo.texelBuffer = VK_NULL_HANDLE;
+              descriptorInfo.texelBuffer = m_common->dummyResources().bufferViewDescriptor();
             }
           } break;
 
@@ -6265,7 +6259,7 @@ namespace dxvk {
               m_cmd->track(res.bufferView->buffer(), (binding.access & vk::AccessWriteMask)
                 ? DxvkAccess::Write : DxvkAccess::Read);
             } else {
-              descriptorInfo.texelBuffer = VK_NULL_HANDLE;
+              descriptorInfo.texelBuffer = m_common->dummyResources().bufferViewDescriptor();
             }
           } break;
 
@@ -6280,9 +6274,7 @@ namespace dxvk {
 
               m_cmd->track(res.bufferSlice.buffer(), DxvkAccess::Read);
             } else {
-              descriptorInfo.buffer.buffer = VK_NULL_HANDLE;
-              descriptorInfo.buffer.offset = 0;
-              descriptorInfo.buffer.range = VK_WHOLE_SIZE;
+              descriptorInfo.buffer = m_common->dummyResources().bufferDescriptor();
             }
           } break;
 
@@ -6298,9 +6290,7 @@ namespace dxvk {
               m_cmd->track(res.bufferSlice.buffer(), (binding.access & vk::AccessWriteMask)
                 ? DxvkAccess::Write : DxvkAccess::Read);
             } else {
-              descriptorInfo.buffer.buffer = VK_NULL_HANDLE;
-              descriptorInfo.buffer.offset = 0;
-              descriptorInfo.buffer.range = VK_WHOLE_SIZE;
+              descriptorInfo.buffer = m_common->dummyResources().bufferDescriptor();
             }
           } break;
 
@@ -6667,7 +6657,7 @@ namespace dxvk {
 
         m_cmd->track(m_state.vi.vertexBuffers[binding].buffer(), DxvkAccess::Read);
       } else {
-        buffers[i] = VK_NULL_HANDLE;
+        buffers[i] = m_device->features().extRobustness2.nullDescriptor ? VK_NULL_HANDLE : m_common->dummyResources().bufferHandle();
         offsets[i] = 0;
         lengths[i] = 0;
         strides[i] = 0;
